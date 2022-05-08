6ef3196ecbfbab3dd3805659ec22484e57f908f2
Sudhanshu-0907 <sudhanshusingh0907@gmail.com>
2022-05-08 22:05:28 +0530

    Updates

---
+++ precache-manifest.46a88d33ddfd99d2f0043973fdc87b49.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd80b6ebafac1b513ea7f9ade472fee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97d2342d8bd0ff1f5482",
+    "url": "/static/js/2.dc837827.chunk.js"
+  },
+  {
+    "revision": "410360b0be15276762d7",
+    "url": "/static/js/main.a9e18733.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
